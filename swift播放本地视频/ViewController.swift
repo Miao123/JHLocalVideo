64a9d11335db52066345218cd029788ebbe3ec39
Miao123 <mac@macdeiMac.local>
2015-07-18 21:59:30 +0800

    swift
    swift

--- swift播放本地视频/ViewController.swift
+++ swift播放本地视频/ViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import MediaPlayer
+import MobileCoreServices
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    var pc:MPMoviePlayerViewController!
+    var picker:UIImagePickerController!
+    var videoUrl:NSURL?
+    var player:MPMoviePlayerViewController!
+    @IBAction func PlayVideo(sender: AnyObject) {
+        if let url = videoUrl{
+            player = MPMoviePlayerViewController(contentURL: url);presentViewController(player, animated: true, completion: nil)
+        }else{
+            var alert = UIAlertView()
+            alert.title = "没有录制视频"
+            alert.addButtonWithTitle("确定")
+            alert.addButtonWithTitle("OK")
+            alert.show()
+        }
+    }
+    @IBAction func StartRecording(sender: AnyObject) {
+        picker = UIImagePickerController()
+        picker.mediaTypes = [kUTTypeMovie!]
+        picker.sourceType = UIImagePickerControllerSourceType.Camera
+        picker.cameraCaptureMode = UIImagePickerControllerCameraCaptureMode.Video
+        picker.delegate = self
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        videoUrl = info[UIImagePickerControllerMediaURL] as? NSURL
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+//    播放本地视频
+    @IBAction func button(sender: AnyObject) {
+        print("dd")
+        pc = MPMoviePlayerViewController(contentURL: NSBundle.mainBundle().URLForResource("popeye", withExtension: "mp4"))
+        presentViewController(pc, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
